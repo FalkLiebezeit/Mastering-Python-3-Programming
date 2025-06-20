7cb64fa8485be137e5750586e815fc554a16cb9a
@FalkLiebezeit <falk.liebezeit@outlook.com>
2025-06-20 19:31:14 +0200

    200625

---
+++ Excel/Create numbers in Rows 2.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+# Datei öffnen oder erstellen
+datei = "C:\\Users\\Falk\\TestData\\CreateRows.xlsx"
+
+# Eine Liste mit 10 Zahlen erstellen
+zahlen = list(range(1, 11))
+
+# Einen DataFrame mit mindestens 1 Zeile und 3 Spalten erstellen
+df = pd.DataFrame(index=range(1), columns=["A", "B", "C"]) 
+
+# Zahlen in Spalte C (Index 3) schreiben
+for i, zahl in enumerate(zahlen, start=1):
+    #ws.cell(row=i, column=3, value=zahl)  # Spalte C entspricht Index 3
+    df.loc[i, "C"] = zahl  # Beispielwert 42
+
+    # Wert in Zelle C1 setzen
+    # df.loc[0, "C"] = 42  # Beispielwert 42
+
+
+
+# Datei speichern
+df.to_excel(datei, index=False)
+
+print("Der Wert wurde erfolgreich in Zelle C1 gespeichert!")
