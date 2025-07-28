bfae00aaedc2e928b271cb62dd604efc42ee833e
@FalkLiebezeit <falk.liebezeit@outlook.com>
2025-07-28 21:35:20 +0200

    280725

--- Basic language structure/Arrays/list aliasing.py
+++ Basic language structure/Arrays/list aliasing.py
@@ -1,8 +1,19 @@
-'''Program E7x9.py'''
-# list aliasing
-list1=[1, 2, 3, 4, 5]
-list2=list1
-print(list2)
+"""Demonstration of list aliasing in Python.
+
+Shows that assigning one list to another variable creates an alias,
+so changes to one affect the other.
+"""
+
+# Create a list and assign it to another variable (aliasing)
+list1 = [1, 2, 3, 4, 5]
+list2 = list1  # list2 is an alias for list1
+
+print("Initial list2:", list2)
+
+# Append an element to list1
 list1.append(16)
-print(list1)
-print(list2)
+
+# Both list1 and list2 reflect the change, since they reference the same object
+print("After appending to list1:")
+print("list1:", list1)
+print("list2:", list2)
--- Basic language structure/bool operator/04_demorgan.py
+++
@@ -1,9 +0,0 @@
-#04_demorgan.py
-print("x1 \t x2 \t y1 \t y2 \t y3 \t y4")
-for x1 in False, True:
-    for x2 in False, True:
-        y1=not(x1 and x2)
-        y2=not x1 or not x2
-        y3=not(x1 or x2)
-        y4=not x1 and not x2
-        print(x1,x2,y1,y2,y3,y4,sep='\t')
